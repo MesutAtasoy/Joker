b89ef1468b695647b88bd3736d72c6b44f9149f2
Mesut Atasoy <mesutatasoy15@gmail.com>
2021-07-03 22:16:47 +0300

    Serilog added Merchant Api

--- src/Services/Merchant/Merchant.Api/Program.cs
+++ src/Services/Merchant/Merchant.Api/Program.cs
@@ -1,17 +1,75 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Merchant.Api
 {
     public class Program
     {
+       /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            Log.Logger = CreateSerilogLogger(configuration, "Merchant.Api");
+
+            try
+            {
+                Log.Information("Application starting up...");
+
+                CreateHostBuilder(args)
+                    .Build()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The application failed to start correctly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
+        /// <summary>
+        /// Creates Host Builder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+        /// <summary>
+        /// Returns configuration with environment
+        /// </summary>
+        /// <returns></returns>
+        private static IConfiguration GetConfiguration()
+        {
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+
+        public static ILogger CreateSerilogLogger(IConfiguration configuration, string applicationName)
+        {
+            return new LoggerConfiguration()
+                .Enrich.WithProperty("ApplicationContext", applicationName)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
     }
 }
