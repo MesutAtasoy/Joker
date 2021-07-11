dc93b12d5bd11a40434ebdfb97a9aae1d610be23
mesut <mesutatasoy15@gmail.com>
2021-07-11 20:21:19 +0300

    Docker Files Updated

---
+++ src/Aggregators/Aggregator.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aggregator.Api.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/HealthCheck")]
+    public class HealthCheckController :  ControllerBase
+    {
+
+        [HttpGet("api-status")]
+        [HttpHead("api-status")]
+        public ActionResult ApiStatus()
+        {
+            return Ok("Aggregator Api is awake!");
+        }
+    }
+}
