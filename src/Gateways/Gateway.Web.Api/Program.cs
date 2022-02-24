5b150261cbe11a832ba0c0d0534c7786a40b9b27
Mesut <mesutatasoy15@gmail.com>
2022-02-24 21:26:29 +0300

    Gateway.Web.Api Refactored

--- src/Gateways/Gateway.Web.Api/Program.cs
+++ src/Gateways/Gateway.Web.Api/Program.cs
@@ -1,85 +1,56 @@
+using Gateway.Web.Api.Extensions;
 using Joker.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
 using Serilog;
-using ILogger = Serilog.ILogger;
+using ConfigurationExtensions = Gateway.Web.Api.Extensions.ConfigurationExtensions;
 
+var configuration = ConfigurationExtensions.GetConfiguration();
 
-namespace Gateway.Web.Api;
-
-public class Program
+Log.Logger = LoggerBuilder.CreateLoggerElasticSearch(x =>
 {
-    /// <summary>
-    /// Main
-    /// </summary>
-    /// <param name="args"></param>
-    public static void Main(string[] args)
-    {
-        var configuration = GetConfiguration();
-        Log.Logger = CreateSerilogLogger(configuration, "Gateway.Web.Api");
+    x.Url = configuration["elk:url"];
+    x.BasicAuthEnabled = false;
+    x.IndexFormat = "joker-logs";
+    x.AppName = "Gateway.Web.Api";
+    x.Enabled = true;
+});
 
-        try
-        {
-            Log.Information("Application starting up...");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    
+    var services = builder.Services;
+    services.AddControllers();
+            
+    services.AddOcelot(configuration)
+        .AddConsul()
+        .AddPolly();
+            
+    services.AddCors(options => options.AddDefaultPolicy(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+    services.AddSwaggerForOcelot(configuration);
+    services.AddJokerOpenTelemetry(configuration);
+    
+    var app = builder.Build();
+    
+    if (app.Environment.IsDevelopment())
+        app.UseDeveloperExceptionPage();
 
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "The application failed to start correctly.");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
+    app.UseCors();
+    app.UseSwaggerForOcelotUI();
+    app.UseRouting();
+    app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    app.UseOcelot().Wait();
 
-    /// <summary>
-    /// Creates Host Builder
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="args"></param>
-    /// <returns></returns>
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-            .ConfigureAppConfiguration((hostingContext, config) =>
-            {
-                config
-                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                    .AddJsonFile("appsettings.json", true, true)
-                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
-                    .AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
-                    .AddEnvironmentVariables();
-            })
-            .UseSerilog();
-
-    /// <summary>
-    /// Returns configuration with environment
-    /// </summary>
-    /// <returns></returns>
-    private static IConfiguration GetConfiguration()
-    {
-        string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
-            .AddEnvironmentVariables();
-
-        return builder.Build();
-    }
-
-    private static ILogger CreateSerilogLogger(IConfiguration configuration, string applicationName)
-    {
-        return LoggerBuilder.CreateLoggerElasticSearch(x =>
-        {
-            x.Url = configuration["elk:url"];
-            x.BasicAuthEnabled = false;
-            x.IndexFormat = "joker-logs";
-            x.AppName = applicationName;
-            x.Enabled = true;
-        });
-    }
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "The application failed to start correctly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
